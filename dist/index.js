479e92c134f60203fba914dd61065d27c07dcd72
Eria Othieno Pinyi <pinyito@users.noreply.github.com>
2022-08-28 02:39:59 -0400

    Basic functionality of to do list

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,43 @@
 "use strict";
-function printDouble(msg) {
-    console.log(msg);
-    console.log(msg);
+;
+const btn = document.getElementById('btn');
+const input = document.getElementById('todoinput');
+const form = document.querySelector('form');
+const list = document.getElementById('todolist');
+const todos = readTodos();
+todos.forEach(createTodo);
+function readTodos() {
+    const todosJSON = localStorage.getItem('todos');
+    if (todosJSON === null)
+        return [];
+    return JSON.parse(todosJSON);
 }
-printDouble("Hello my students!");
+function saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+;
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const newTodo = {
+        text: input.value,
+        status: false
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+    saveTodos();
+    input.value = '';
+});
+function createTodo(todo) {
+    const newListElement = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = todo.status;
+    checkbox.addEventListener('change', function () {
+        todo.status = checkbox.checked;
+        saveTodos();
+    });
+    newListElement.append(todo.text);
+    newListElement.append(checkbox);
+    list.append(newListElement);
+}
+;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,50 @@
-function printDouble(msg: string): void{
-    console.log(msg);
-    console.log(msg);
+
+interface Todo {
+    text: string;
+    status: boolean;
+};
+
+const btn = document.getElementById('btn')! as HTMLButtonElement;
+const input = document.getElementById('todoinput')! as HTMLInputElement;
+const form = document.querySelector('form')!;
+const list = document.getElementById('todolist')!;
+
+const todos: Todo[] = readTodos();
+todos.forEach(createTodo);
+
+function readTodos(): Todo[]{
+    const todosJSON = localStorage.getItem('todos');
+    if(todosJSON === null) return [];
+    return JSON.parse(todosJSON);
 }
-printDouble("Hello my students!");
+
+function saveTodos(){
+    localStorage.setItem('todos', JSON.stringify(todos));
+};
+
+form.addEventListener('submit', function (e: SubmitEvent) {
+        e.preventDefault();
+        const newTodo: Todo = {
+            text: input.value,
+            status: false
+        };
+        createTodo(newTodo);
+        todos.push(newTodo);
+        
+        saveTodos();
+        input.value = '';
+    });
+
+    function createTodo(todo: Todo){
+        const newListElement = document.createElement('li');
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.checked = todo.status;
+        checkbox.addEventListener('change', function(){
+            todo.status = checkbox.checked;
+            saveTodos();
+        });
+        newListElement.append(todo.text);
+        newListElement.append(checkbox);
+        list.append(newListElement);
+    };
